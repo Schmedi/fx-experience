b7e0082d5ed1d8ed32400671b643d709a5040ed8
Patrick Schmid <patrick_schmid@gmx.net>
2014-10-10 10:55:20 +0200

    Schiller FX-Keyboard

--- fx-onscreen-keyboard/src/main/java/org/comtel/javafx/xml/layout/Keyboard.java
+++ fx-onscreen-keyboard/src/main/java/org/comtel/javafx/xml/layout/Keyboard.java
@@ -2,7 +2,7 @@
 // Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.7 generiert 
 // Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2014.06.19 um 12:28:24 PM CEST 
+// Generiert: 2014.10.10 um 10:47:40 AM CEST 
 //
 
 
--- fx-onscreen-keyboard/src/main/java/org/comtel/samples/MainDemo.java
+++ fx-onscreen-keyboard/src/main/java/org/comtel/samples/MainDemo.java
@@ -2,156 +2,61 @@ package org.comtel.samples;
 
 import java.util.Locale;
 
-import javax.swing.ButtonGroup;
-
-import javafx.animation.Animation;
-import javafx.animation.FadeTransition;
-import javafx.animation.ParallelTransition;
-import javafx.animation.ScaleTransition;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.Separator;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextInputControl;
-import javafx.scene.control.ToolBar;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-
-import org.comtel.javafx.control.KeyBoardPopup;
-import org.comtel.javafx.control.KeyBoardPopupBuilder;
+import org.comtel.javafx.control.DefaultLayers;
+import org.comtel.javafx.control.KeyBoardBuilder;
+import org.comtel.javafx.control.KeyboardPane;
 import org.comtel.javafx.robot.RobotFactory;
 
-public class MainDemo extends Application {
+public class MainDemo extends Application
+{
 
-	private Animation fadeAnimation;
+  private KeyboardPane keyboard;
 
-	private KeyBoardPopup popup;
+  @Override
+  public void start(Stage stage)
+  {
 
-	@Override
-	public void start(Stage stage) {
+    stage.setTitle("FX Keyboard (" + System.getProperty("javafx.runtime.version") + ")");
+    stage.setResizable(true);
+    //NUMBLOCK examples
+    keyboard = KeyBoardBuilder.create().layer(DefaultLayers.DEFAULT).initScale(1).initLocale(Locale.forLanguageTag("fr")).addIRobot(RobotFactory.createFXRobot()).build();
 
-		stage.setTitle("FX Keyboard (" + System.getProperty("javafx.runtime.version") + ")");
-		stage.setResizable(true);
+    StackPane outerStackPane = new StackPane();
+    StackPane innerStackPane = new StackPane();
+    VBox pane = new VBox(20);
 
-		popup = KeyBoardPopupBuilder.create().initScale(1.0).initLocale(Locale.ENGLISH).addIRobot(RobotFactory.createFXRobot()).build();
-		popup.getKeyBoard().setOnKeyboardCloseButton(event -> setPopupVisible(false, null));
-		
-		VBox pane = new VBox(20);
+    TextField initialFocusTF = new TextField("");
+    pane.getChildren().add(new Label(""));
+    pane.getChildren().add(new Label("Text Field"));
+    pane.getChildren().add(initialFocusTF);
+    pane.getChildren().add(new Label("Text Area"));
+    pane.getChildren().add(new TextArea(""));
 
-		Button okButton = new Button("Ok");
-		okButton.setDefaultButton(true);
+    HBox hPane = new HBox();
 
-		Button cancelButton = new Button("Cancel");
-		cancelButton.setCancelButton(true);
+    hPane.getChildren().add(keyboard);
+    pane.getChildren().add(hPane);
+    outerStackPane.getChildren().add(innerStackPane);
+    innerStackPane.getChildren().add(pane);
+    Scene scene = new Scene(outerStackPane, 740, 590);
 
-		CheckBox spaceKeyMoveCb = new CheckBox("Movable");
-		spaceKeyMoveCb.setSelected(true);
-		popup.getKeyBoard().spaceKeyMoveProperty().bind(spaceKeyMoveCb.selectedProperty());
-		
-		pane.getChildren().add(new ToolBar(okButton, cancelButton, spaceKeyMoveCb));
-		pane.getChildren().add(new Label("Text1"));
-		pane.getChildren().add(new TextField(""));
-		pane.getChildren().add(new TextArea(""));
-		pane.getChildren().add(new Label("Password"));
-		pane.getChildren().add(new PasswordField());
-		
-		
+    stage.setOnCloseRequest((event) -> System.exit(0));
+    stage.setScene(scene);
+    stage.show();
+    initialFocusTF.requestFocus();
+  }
 
-		// pane.getChildren().add(KeyBoardBuilder.create().addIRobot(RobotFactory.createFXRobot()).build());
-
-		Scene scene = new Scene(pane, 600, 400);
-
-		// add keyboard scene listener to all text components
-		scene.focusOwnerProperty().addListener((value, n1, n2) -> {
-			if (n2 != null && n2 instanceof TextInputControl) {
-				setPopupVisible(true, (TextInputControl) n2);
-			} else {
-				setPopupVisible(false, null);
-			}
-		});
-
-		// add double click listener
-		stage.addEventFilter(MouseEvent.MOUSE_CLICKED, event -> {
-			if (event.getClickCount() == 2) {
-				Node node = scene.getFocusOwner();
-				if (node != null && node instanceof TextInputControl) {
-					setPopupVisible(true, (TextInputControl) node);
-				}
-			}
-		});
-
-		stage.setOnCloseRequest((event) -> System.exit(0));
-
-		stage.setScene(scene);
-		popup.show(stage);
-		stage.show();
-
-	}
-
-	private void setPopupVisible(final boolean b, final TextInputControl textNode) {
-
-		Platform.runLater(() -> {
-			if (b && textNode != null) {
-				Rectangle2D textNodeBounds = new Rectangle2D(textNode.getScene().getWindow().getX() + textNode.getLocalToSceneTransform().getTx(), textNode.getScene().getWindow().getY()
-						+ textNode.getLocalToSceneTransform().getTy(), textNode.getWidth(), textNode.getHeight());
-
-				Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-				if (textNodeBounds.getMinX() + popup.getWidth() > screenBounds.getMaxX()) {
-					popup.setX(screenBounds.getMaxX() - popup.getWidth());
-				} else {
-					popup.setX(textNodeBounds.getMinX());
-				}
-
-				if (textNodeBounds.getMaxY() + popup.getHeight() > screenBounds.getMaxY()) {
-					popup.setY(textNodeBounds.getMinY() - popup.getHeight() + 20);
-				} else {
-					popup.setY(textNodeBounds.getMaxY() + 40);
-				}
-			}
-
-			if (fadeAnimation != null) {
-				fadeAnimation.stop();
-			}
-			if (!b) {
-				popup.hide();
-				return;
-			}
-			if (popup.isShowing()) {
-				return;
-			}
-			popup.getKeyBoard().setOpacity(.0);
-
-			FadeTransition fade = new FadeTransition(Duration.seconds(.5), popup.getKeyBoard());
-			fade.setToValue(b ? 1. : .8);
-			// fade.setOnFinished((event) -> fadeAnimation = null);
-
-			ScaleTransition scale = new ScaleTransition(Duration.seconds(.5), popup.getKeyBoard());
-			scale.setToX(b ? 1. : .8);
-			scale.setToY(b ? 1. : .8);
-
-			ParallelTransition tx = new ParallelTransition(fade, scale);
-			fadeAnimation = tx;
-			tx.play();
-			if (b && !popup.isShowing()) {
-				popup.show(popup.getOwnerWindow());
-			}
-
-		});
-	}
-
-	public static void main(String[] args) {
-		Application.launch(args);
-	}
-
+  public static void main(String[] args)
+  {
+    Application.launch(args);
+  }
 }
